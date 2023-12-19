02b5e3a3c506b5067b0bf208907532723683a868
Teo Hughes <th1522@ic.ac.uk>
2023-12-19 20:00:43 +0000

    adding reactQuery to platforms and genres

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "9d92380d5b614f0299951b7cea8800b0",
   },
 });
